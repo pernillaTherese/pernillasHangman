d4f3239992e665a2955d7d6794f24b9a72d445a7
pernillaTherese <pernilla.t.andersson@gmail.com>
2021-11-16 22:48:10 +0100

    Closing for the day

--- app/src/main/java/com/pernillatherese/hangman/PlayActivity.java
+++ app/src/main/java/com/pernillatherese/hangman/PlayActivity.java
@@ -2,13 +2,40 @@ package com.pernillatherese.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class PlayActivity extends AppCompatActivity {
+    AnimationDrawable firstAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        ImageView animView = findViewById(R.id.anim_view);
+        animView.setBackgroundResource(R.drawable.first_animation);
+        firstAnimation = (AnimationDrawable) animView.getBackground();
+
+        //Keyboard setting
+        EditText guessEdit = findViewById(R.id.guess_edit);
+        KeyBoard keyboard = findViewById(R.id.keyboard);
+        guessEdit.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        guessEdit.setTextIsSelectable(true);
+
+        InputConnection ic = guessEdit.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        firstAnimation.start();
     }
 }
--- app/src/main/java/com/pernillatherese/hangman/StartActivity.java
+++ app/src/main/java/com/pernillatherese/hangman/StartActivity.java
@@ -2,7 +2,10 @@ package com.pernillatherese.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // Go to play Activity
+        final Button playBtn = findViewById(R.id.play_btn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PlayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //TODO: About_btn intent to info view - Fragment or Activity?
+
     }
 }
